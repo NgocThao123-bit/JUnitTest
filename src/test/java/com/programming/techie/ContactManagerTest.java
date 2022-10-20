302a617d8da9d499d3e6def6f7f8659acecdb916
NgocThao123-bit <73955710+NgocThao123-bit@users.noreply.github.com>
2022-10-20 09:49:56 -0400

    Complete Parameterized tests

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -3,6 +3,13 @@ package com.programming.techie;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -62,6 +69,49 @@ class ContactManagerTest {
     public void shouldTestContactCreationOnDEV() {
         Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")));
         contactManager.addContact("John", "Doe", "0988885456");
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+    @DisplayName("Repeat Contact Creation Test 5 Times")
+    @RepeatedTest(5)
+    public void shouldTestContactCreationRepeatedly() {
+        contactManager.addContact("John", "Doe", "0988885456");
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+//    @Test
+//    @DisplayName("Phone Number should start with 0")
+//    public void shouldTestPhoneNumberFormat() {
+//        contactManager.addContact("John", "Doe", "0988885456");
+//        assertFalse(contactManager.getAllContacts().isEmpty());
+//        assertEquals(1, contactManager.getAllContacts().size());
+//    }
+
+//    @DisplayName("Phone Number should match the required Format")
+//    @ParameterizedTest
+//    @ValueSource(strings = {"0988885456", "988885456", "+0988885456"})
+//    public void shouldTestPhoneNumberFormat(String phoneNumber) {
+//        contactManager.addContact("John", "Doe", phoneNumber);
+//        assertFalse(contactManager.getAllContacts().isEmpty());
+//        assertEquals(1, contactManager.getAllContacts().size());
+//    }
+//    @DisplayName("Method Source Case - Phone Number should match the required Format")
+//    @ParameterizedTest
+//    @MethodSource("phoneNumberList")
+//    public void shouldTestPhoneNumberFormatUsingMethodSource(String phoneNumber) {
+//        contactManager.addContact("John", "Doe", phoneNumber);
+//        assertFalse(contactManager.getAllContacts().isEmpty());
+//        assertEquals(1, contactManager.getAllContacts().size());
+//    }
+//
+//    private static List<String> phoneNumberList() {
+//        return Arrays.asList("0123456789", "01234567890", "0123456789");
+//    }
+    @DisplayName("CSV Source Case - Phone Number should match the required Format")
+    @ParameterizedTest
+    @CsvSource({"0123456789", "2,1234567890","3,+0123456789"})
+    public void shouldTestPhoneNumberFormatUsingCSVSource(String phoneNumber) {
+        contactManager.addContact("John", "Doe", phoneNumber);
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
     }
